cfc54295b731a16a1c5d86db77f5d7d0d03b835d
sai <sai>
2023-06-21 17:18:31 +0530

    cool

--- 2d_arrays.c
+++ 2d_arrays.c
@@ -2,9 +2,11 @@
 
 int main() {
   int arr[][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  // we can see the indices using the clangd extension
+  sadjhsajdhasjdhsajhdasjk
+      // we can see the indices using the clangd extension
 
-  int i, g;
+      int i,
+      g;
   int arr1[][g] = {};
   return 0;
 }
