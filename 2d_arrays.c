850633c5be9ee2e343d36d39b1100184d5cb3a4c
kenchakkagariprathyusha <131856499+kenchakkagariprathyusha@users.noreply.github.com>
2023-06-22 10:20:49 +0530

    pra

--- 2d_arrays.c
+++ 2d_arrays.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
+//hi everyone
+//heee
+//wjvcwmcw
 int main() {
   int arr[][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  sadjhsajdhasjdhsajhdasjk
+  //sadjhsajdhasjdhsajhdasjk
       // we can see the indices using the clangd extension
 
-      int i,
-      g;
+      int i,g;
   int arr1[][g] = {};
   return 0;
 }
